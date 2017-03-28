e784df53384ab1c513e9b5f94d8ac34aec348bed
Jeremy Liu <liujiong@gohighsec.com>
2017-03-28 20:30:59 +0800

    Add tox and pep8 check
    Signed-off-by: Jeremy Liu <liujiong@gohighsec.com>

--- mnist/test_mnist.py
+++ mnist/test_mnist.py
@@ -1,29 +1,35 @@
 import tensorflow as tf
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 
-# Train
-mnist = input_data.read_data_sets("./mnist_data/", one_hot=True)
 
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+def main():
+    # Train
+    mnist = input_data.read_data_sets("./mnist_data/", one_hot=True)
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
 
-y_ = tf.placeholder("float", [None, 10])
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+    y_ = tf.placeholder("float", [None, 10])
+    cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 
-for i in range(1000):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    train_step = tf.train.GradientDescentOptimizer(0.01).minimize(
+        cross_entropy)
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+
+    for i in range(1000):
+      batch_xs, batch_ys = mnist.train.next_batch(100)
+      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    # Test and predict
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
 
 
-# Test and predict
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+if __name__ == '__main__':
+    main()
